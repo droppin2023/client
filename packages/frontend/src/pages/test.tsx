1fe7cc12893a6425c94b2d528f4d89a241911cd4
Phia(가영) <76558220+rkdud007@users.noreply.github.com>
2023-01-19 19:21:42 +0800

    abi finish

--- packages/frontend/src/pages/test.tsx
+++ packages/frontend/src/pages/test.tsx
@@ -1,9 +1,10 @@
 import { useContract } from 'wagmi'
-import badgeContractAbi from '../../config/abis/BadgeFacet.json'
+import badgeContractAbi from '../config/abis/BadgeFacet.json'
 
+import type { NextPage } from 'next'
 import 'twin.macro'
 
-const TestContractPage = () => {
+const TestContractPage: NextPage = () => {
   const badgeContract = useContract({
     address: '0x1c8aBb04C8Ab9a105ee2c44b13398E98fC12E6d4',
     abi: badgeContractAbi,
