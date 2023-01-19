bc094c0abdecab4e8c29ace07ccc8e2211bf049d
Phia(가영) <76558220+rkdud007@users.noreply.github.com>
2023-01-19 18:27:46 +0800

    feat: connect web3

---
+++ packages/frontend/src/pages/test.jsx
@@ -0,0 +1,15 @@
+import { useContract } from 'wagmi'
+import badgeContractAbi from '../../config/abis/BadgeFacet.json'
+
+import 'twin.macro'
+
+const TestContractPage = () => {
+  const badgeContract = useContract({
+    address: '0x1c8aBb04C8Ab9a105ee2c44b13398E98fC12E6d4',
+    abi: badgeContractAbi,
+  })
+  console.log(badgeContract)
+  return <div></div>
+}
+
+export default TestContractPage
