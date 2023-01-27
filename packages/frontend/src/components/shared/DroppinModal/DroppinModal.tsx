d3e3475bba01482e7ba59e7f997c8da47c166cb4
metalboyrick <rsulisthio@gmail.com>
2023-01-27 18:22:31 +0800

    fix: loading modal with spinner

--- packages/frontend/src/components/shared/DroppinModal/DroppinModal.tsx
+++ packages/frontend/src/components/shared/DroppinModal/DroppinModal.tsx
@@ -1,10 +1,9 @@
 import {
   Modal,
   ModalBody,
-  ModalCloseButton,
   ModalContent,
-  ModalHeader,
   ModalOverlay,
+  Spinner,
   Text,
   VStack,
 } from '@chakra-ui/react'
@@ -12,10 +11,9 @@ import {
 import { background2 } from '@constants/colors'
 import { ONE_QUEST_DETAIL } from '@mockData'
 
-import type { DroppinModalProps } from './DroppinModal.types'
 import Done from '@components/icons/Done'
 import Fail from '@components/icons/Fail'
-import Loading from '@components/icons/Loading'
+import type { DroppinModalProps } from './DroppinModal.types'
 
 const DroppinModal = ({ isOpen, onClose, modatMessage, modalStatus }: DroppinModalProps) => {
   // TODO: fetch the quest details here
@@ -25,22 +23,16 @@ const DroppinModal = ({ isOpen, onClose, modatMessage, modalStatus }: DroppinMod
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent bg={background2}>
-        <ModalHeader>
-          {' '}
-          <ModalCloseButton />
-        </ModalHeader>
-
         <ModalBody pb={6}>
-          <VStack spacing={2} align="center" mt={4}></VStack>
-          {modalStatus === 0 ? (
-            <Loading position="absolute" right="-12px" top="-12px" width="28px" height="28px" />
-          ) : modalStatus === 1 ? (
-            <Done position="absolute" right="-12px" top="-12px" width="28px" height="28px" />
-          ) : (
-            <Fail position="absolute" right="-12px" top="-12px" width="28px" height="28px" />
-          )}
+          <VStack spacing={10} align="center" mt={4}>
+            {modalStatus === 0 ? (
+              <Spinner size="xl" />
+            ) : modalStatus === 1 ? (
+              <Done width="48px" height="48px" />
+            ) : (
+              <Fail />
+            )}
 
-          <VStack align="center" mt={4}>
             <Text>{modatMessage}</Text>
           </VStack>
         </ModalBody>
