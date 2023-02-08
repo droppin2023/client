74ece2e30a263c5275214259711c6fd16d2e8189
metalboyrick <rsulisthio@gmail.com>
2023-02-08 15:12:55 +0800

    remove user requirement in discord connect community

--- packages/frontend/src/components/shared/ConnectDiscordModal/ConnectDiscordModal.tsx
+++ packages/frontend/src/components/shared/ConnectDiscordModal/ConnectDiscordModal.tsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   Button,
   FormControl,
   FormHelperText,
@@ -12,11 +11,10 @@ import {
   ModalHeader,
   ModalOverlay,
   Select,
-  Text,
 } from '@chakra-ui/react'
 import { ConnectDiscordModalProps } from './ConnectDiscordModal.props'
 
-import { background2, primary, primaryHighlight, secondary, secondaryWeak } from '@constants/colors'
+import { background2, primary, primaryHighlight } from '@constants/colors'
 import { DiscordGuild } from '@queries/common'
 import * as globalSty from '@styles'
 import { ChangeEvent, useState } from 'react'
@@ -47,55 +45,39 @@ const ConnectDiscordModal = (props: ConnectDiscordModalProps) => {
       <ModalContent bg={background2}>
         <ModalHeader>Connect Server to Discord</ModalHeader>
         <ModalBody>
-          <FormControl>
-            <FormLabel>Discord Account</FormLabel>
-            <FormHelperText css={[globalSty.helperText]}>
-              Your connected discord account
-            </FormHelperText>
-            {(discordUser?.id || '').length > 0 ? (
-              <Box padding="8px 12px" borderRadius="8px" bg={secondaryWeak}>
-                {discordUser.name}
-                <Text as="span" color={secondary}>{`#${discordUser.discriminator}`}</Text>
-              </Box>
-            ) : (
-              <>{/* TODO: implement connect discord*/}</>
-            )}
-          </FormControl>
-          {(discordUser?.id || '').length > 0 && (
-            <>
-              <FormControl mt={4}>
-                <FormLabel>Select Server to Connect</FormLabel>
-                <FormHelperText css={[globalSty.helperText]}>
-                  Select a server to attach to this community
-                </FormHelperText>
-                <Select variant="filled" placeholder="Select server" onChange={handleSelectGuild}>
-                  {guilds.map((item) => {
-                    const value = {
-                      guildId: item.id,
-                      name: item.name,
-                    }
+          <>
+            <FormControl mt={4}>
+              <FormLabel>Select Server to Connect</FormLabel>
+              <FormHelperText css={[globalSty.helperText]}>
+                Select a server to attach to this community
+              </FormHelperText>
+              <Select variant="filled" placeholder="Select server" onChange={handleSelectGuild}>
+                {guilds.map((item) => {
+                  const value = {
+                    guildId: item.id,
+                    name: item.name,
+                  }
 
-                    return (
-                      <option key={item.id} value={JSON.stringify(value)}>
-                        {item.name}
-                      </option>
-                    )
-                  })}
-                </Select>
-              </FormControl>
-              <FormControl mt={4}>
-                <FormLabel>Enter invite link</FormLabel>
-                <FormHelperText css={[globalSty.helperText]}>
-                  Make sure the invite link matches and does not expire!
-                </FormHelperText>
-                <Input
-                  value={link}
-                  onChange={(e) => setLink(e.target.value)}
-                  placeholder="https://discord.gg/your-invite-link"
-                />
-              </FormControl>
-            </>
-          )}
+                  return (
+                    <option key={item.id} value={JSON.stringify(value)}>
+                      {item.name}
+                    </option>
+                  )
+                })}
+              </Select>
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Enter invite link</FormLabel>
+              <FormHelperText css={[globalSty.helperText]}>
+                Make sure the invite link matches and does not expire!
+              </FormHelperText>
+              <Input
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+                placeholder="https://discord.gg/your-invite-link"
+              />
+            </FormControl>
+          </>
         </ModalBody>
         <ModalFooter>
           <Button bg={primary} _hover={{ bg: primaryHighlight }} onClick={handleSubmit}>
