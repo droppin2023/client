aa6885b286e307269420f87f842fdb8454411318
Kenk <kenghin_lim@hotmail.com>
2023-03-05 08:29:30 -0700

    feat: update backend url

--- packages/frontend/src/constants/serverConfig.ts
+++ packages/frontend/src/constants/serverConfig.ts
@@ -1,5 +1,5 @@
 import { env } from '@shared/environment'
 
-export const SERVER_URL = env.backendUrl
+export const SERVER_URL = 'https://stingray-app-elafw.ondigitalocean.app'
 export const DISCORD_ENDPOINT = 'https://discord.com/api'
 export const POLYGON_ID_ENDPOINT = 'https://api-staging.polygonid.com/v1'
