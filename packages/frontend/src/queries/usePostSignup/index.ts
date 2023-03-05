03405a67f309a9841290e3ce32b3f22e89d0aadb
Kenk <kenghin_lim@hotmail.com>
2023-03-05 08:30:55 -0700

    feat: add game7 integration

--- packages/frontend/src/queries/usePostSignup/index.ts
+++ packages/frontend/src/queries/usePostSignup/index.ts
@@ -1,7 +1,37 @@
-import { useState } from 'react'
-import axios from 'axios'
-import type { SignupParams } from './usePostSignup.types'
 import { SIGNUP } from './usePostSignup.constants'
+import type { SignupParams } from './usePostSignup.types'
+import axios from 'axios'
+import { useState } from 'react'
+
+async function callSummonApi(walletAddress: string, email: string, discordUserId: string) {
+  const tenantKey = '358368999175618628'
+  const featureKey = 'user.joined'
+  const apiToken = process.env.NEXT_PUBLIC_SUMMON_API
+
+  console.log('Sending', {
+    featureKey,
+    walletAddress,
+    email,
+    tenantKey,
+  })
+
+  return fetch('https://sandbox-api.summon.xyz/v1/xps/job/integration/action', {
+    method: 'POST',
+    headers: {
+      Authentication: `bearer ${apiToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      featureKey,
+      walletAddress,
+      email,
+      tenantKey,
+    }),
+  })
+    .then((x) => x.json())
+    .then((x) => console.log('Response', featureKey, x))
+    .catch((err) => console.warn(err))
+}
 
 const usePostSignup = () => {
   const [isLoading, setIsLoading] = useState(true)
@@ -12,7 +42,7 @@ const usePostSignup = () => {
 
     try {
       const { data, status } = await axios.post(SIGNUP, params)
-
+      await callSummonApi(params.address, 'gayeongparkk@gmail.com', params.username)
       if (status === 200) {
         setIsLoading(false)
         return data.data
